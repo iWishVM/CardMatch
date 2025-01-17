ede4941bc707d72eea367f81ed653a8706a21145
iWishVM <jennie_boucher@hotmail.com>
2025-01-17 20:10:37 +0000

    Add files via upload

---
+++ DBConnection.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) 
+    {
+         throw err;
+    console.log("Error: ",err);
+  }
+  else
+  {
+    console.log("Connected!");
+}
+  
+}
+);
+
